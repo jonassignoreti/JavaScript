8a67b4d5d13c4bad585879bfa388f325ed0516d3
jonassignoreti <jonassignoreti7@gmail.com>
2022-06-10 23:26:32 -0300

    learning

--- js-udemy/course-repository/node/projeto/src/bancoDeDados.js
+++ js-udemy/course-repository/node/projeto/src/bancoDeDados.js
@@ -1,28 +1,28 @@
-const sequence = {
+const sequence = { // contador sequencial
     _id: 1,
     get id() { return this._id++ }
 }
 
-const produtos = {}
+const produtos = {} // simula um banco de dados
 
 function salvarProduto(produto) {
-    if (!produto.id) produto.id = sequence.id
+    if (!produto.id) produto.id = sequence.id // define a id do produto novo
     produtos[produto.id] = produto
     return produto
 }
 
-function getProduto(id) {
+function getProduto(id) { // retorna o produto na posição passada pelo id
     return produtos[id] || {}
 }
 
-function getProdutos() {
+function getProdutos() { // retorna o valor de todos os produtos.
     return Object.values(produtos)
 }
 
 function excluirProduto(id) {
-    const produto = produtos[id]
-    delete produtos[id]
-    return produto
+    const produto = produtos[id] // guarda em uma constante para mostrar no retorno.
+    delete produtos[id] // remove o produto passado por parametro
+    return produto // retorna o produto para visualização
 }
 
-module.exports = { salvarProduto, getProduto, getProdutos, excluirProduto }
+module.exports = { salvarProduto, getProduto, getProdutos, excluirProduto } // permite as funções a serem exportadas do arquivo
--- js-udemy/course-repository/node/projeto/src/servidor.js
+++ js-udemy/course-repository/node/projeto/src/servidor.js
@@ -1,21 +1,21 @@
-const porta = 3003
+const porta = 3003 // definife a porta do servidor no computador
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const bancoDeDados = require('./bancoDeDados')
+const express = require('express') // importa o express
+const app = express() // define o express na constante app
+const bodyParser = require('body-parser') // importa o bodyParser
+const bancoDeDados = require('./bancoDeDados') // importa o module.exports do arquivo bancoDeDados
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true })) //
 
-app.get('/produtos', (req, res, next) => {
+app.get('/produtos', (req, res, next) => { // retorna todos os produtos no banco de dados
     res.send(bancoDeDados.getProdutos())
 })
 
-app.get('/produtos/:id', (req, res, next) => {
+app.get('/produtos/:id', (req, res, next) => { // retorna somente o produto passado como parâmetro através do id.
     res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.post('/produtos', (req, res, next) => {
+app.post('/produtos', (req, res, next) => { // adiciona um produto novo no banco de dados
     const produto = bancoDeDados.salvarProduto({
         nome: req.body.nome,
         preco: req.body.preco

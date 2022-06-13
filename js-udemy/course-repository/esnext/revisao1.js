9922b9d11aab9b37446bda296f09c054e39fab24
jonassignoreti <jonassignoreti7@gmail.com>
2022-06-13 20:55:18 -0300

    learning

--- js-udemy/course-repository/esnext/revisao1.js
+++ js-udemy/course-repository/esnext/revisao1.js
@@ -2,20 +2,20 @@
 {
     var a = 2
     let b = 3
-    console.log(b)
+    console.log(b) // let doesn't have scope of bloc
 }
-console.log(a)
+console.log(a) //var não tem escopo de bloco
 
 // Template String
 const produto = 'iPad'
 console.log(`${produto} é caro!`)
 
 // Destructuring
-const [l, e, ...tras] = "Cod3r"
+const [l, e, ...tras] = "Cod3r" // atribui para 'l' a letra "C", atribui para 'e' a letra "o", e atribui para 'tras' o restante das letras
 console.log(l, e, tras)
 
 const [x, , y] = [1, 2, 3]
 console.log(x, y)
 
-const { idade: i, nome } = { nome: 'Ana', idade: 9 }
+const { idade: i, nome } = { nome: 'Ana', idade: 9 } // destrutura atributos de um objeto
 console.log(i, nome)
